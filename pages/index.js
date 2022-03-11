10e62ea4ecb127869463e2da21a9f7f8f88838b2
otutukingsley <chineduotutu3@gmail.com>
2022-03-11 15:46:31 +0100

    understood dynamic routes, getStaticProps, getStaticPaths and getServerProps

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,19 @@
 import Head from "next/head"
 import Layout, { siteTitle } from "../components/layout"
 import utilStyles from "../styles/utils.module.css"
+import { getSortedPostsData } from "../utils/posts"
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData: allPostsData,
+    },
+  }
+}
+
+export default function Home({ allPostsData }) {
+  console.log(allPostsData)
   return (
     <Layout home>
       <Head>
@@ -19,6 +30,20 @@ export default function Home() {
         <p>
           <strong>Welcome to my first project using NextJs!!!</strong>
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={`${utilStyles.headingLg}`}>Blog</h2>
+        <ul className={`${utilStyles.list}`}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
